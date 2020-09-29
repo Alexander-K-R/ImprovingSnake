6a833291c108deb9309a2c15a35e7d7384a37335
Alex Rodriguez <tug98890@temple.edu>
2020-09-29 10:45:13 -0400

    Java program added so that a developer may clone/pull it from Github to test if the have the necessary software to begin work on 'ImprovingSnake'

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * This program is intended to allow a Github user to clone this
+ * project and run it. This is meant to make the process of
+ * cloning and running the 'Improving Snake' project much smoother
+ * and easier on the developer who wants to work on the project.
+ */
+package HelloWorld;
+
+/**
+ *
+ * @author Alexander Rodriguez
+ */
+public class HelloWorld
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    } 
+}
